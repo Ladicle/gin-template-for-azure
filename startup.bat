2de0249a87b43029692cf7ff54105b7c85ee9603
Ladicle <Ladiclexxx@gmail.com>
2016-06-24 13:55:54 +0900

    Fix gopath and glide package name

--- startup.bat
+++ startup.bat
@@ -1,6 +1,6 @@
 rem Set environment
 set GOROOT=D:\home\site\wwwroot\go
-set GOPATH=D:\home\site
+set GOPATH=D:\home\site\gopath
 set PATH=%PATH%;%GOPATH%\bin;%GOROOT%\bin
 set GO15VENDOREXPERIMENT=1
 
@@ -9,11 +9,20 @@ IF NOT EXIST "%GOROOT%" (
 curl -O https://storage.googleapis.com/golang/go1.6.2.windows-386.zip
 unzip go1.6.2.windows-386.zip
 rm go1.6.2.windows-386.zip
+)
 
-go get github.com/Masterminds/glide
+rem Install Glide
+IF NOT EXIST "%GOPATH%" (
+mkdir %GOPATH%\bin
+cd %GOPATH%\bin
+curl -O https://github.com/Masterminds/glide/releases/download/0.10.2/glide-0.10.2-windows-386.zip
+unzip glide-0.10.2-windows-386.zip
+rm glide-0.10.2-windows-386.zip
 )
 
 rem Move to repository.
+copy D:\home\site\repository %GOPATH%
+rename %GOPATH%\repository %GOPATH%\gin-template-for-azure
 cd D:\home\site\repository
 
 rem Update dependency packages.
